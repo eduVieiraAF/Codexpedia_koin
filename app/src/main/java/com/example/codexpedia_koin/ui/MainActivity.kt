b2d90d699bc7e72c2fa5fc3f38ccc5d9115f9b3d
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-02 06:44:37 -0300

    → gradle updates and corrections → MainActivity

--- app/src/main/java/com/example/codexpedia_koin/ui/MainActivity.kt
+++ app/src/main/java/com/example/codexpedia_koin/ui/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.codexpedia_koin.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.codexpedia_koin.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivityViewModel: MainActivityViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+
+        mainActivityViewModel.githubAccount.observe(this) {
+            tv_content.text = it.toString()
+        }
+
+        btn_fetch.setOnClickListener {
+            mainActivityViewModel.fetchAccount(et_account.text.toString())
+        }
     }
 }
